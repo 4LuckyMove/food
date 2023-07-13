c368b11ed500fb9917ffe0affdbc4ef99dabce60
Maksym <linkoln2896@gmail.com>
2023-07-13 12:35:17 +0300

    create timer and custom date promo

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
+	// Tabs
+
 	const tabs = document.querySelectorAll('.tabheader__item')
 	const tabsContent = document.querySelectorAll('.tabcontent')
 	const tabsParent = document.querySelector('.tabheader__items')
@@ -34,4 +36,68 @@ window.addEventListener('DOMContentLoaded', () => {
 			})
 		}
 	})
+
+	// Timer
+
+	const deadline = '2023-08-01'
+
+	function setEndDate(endDate) {
+		const deadline = new Date(endDate).toLocaleDateString('ru', {
+			day: 'numeric',
+			month: 'long',
+		})
+
+		return deadline
+	}
+
+	function getTimeRemaining(endTime) {
+		const t = Date.parse(endTime) - Date.parse(new Date())
+		const days = Math.floor(t / (1000 * 60 * 60 * 24))
+		const hours = Math.floor((t / (1000 * 60 * 60)) % 24)
+		const minutes = Math.floor((t / (1000 * 60)) % 60)
+		const seconds = Math.floor((t / 1000) % 60)
+
+		return {
+			total: t,
+			days: days,
+			hours: hours,
+			minutes: minutes,
+			seconds: seconds,
+		}
+	}
+
+	function getZero(num) {
+		if (num >= 0 && num < 10) {
+			return `0${num}`
+		} else {
+			return num
+		}
+	}
+
+	function setClock(selector, endTime) {
+		document.querySelector('#deadline').innerHTML = setEndDate(endTime)
+
+		const timer = document.querySelector(selector)
+		const days = timer.querySelector('#days')
+		const hours = timer.querySelector('#hours')
+		const minutes = timer.querySelector('#minutes')
+		const seconds = timer.querySelector('#seconds')
+		const timeInterval = setInterval(updateClock, 1000)
+
+		updateClock()
+
+		function updateClock() {
+			const t = getTimeRemaining(endTime)
+			days.innerHTML = getZero(t.days)
+			hours.innerHTML = getZero(t.hours)
+			minutes.innerHTML = getZero(t.minutes)
+			seconds.innerHTML = getZero(t.seconds)
+
+			if (t.total <= 0) {
+				clearInterval(timeInterval)
+			}
+		}
+	}
+
+	setClock('.timer', deadline)
 })
