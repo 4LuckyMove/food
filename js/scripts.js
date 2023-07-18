eae09d7eee8b857d09409ba015772c8ce0c68ac5
Maksym <linkoln2896@gmail.com>
2023-07-18 14:25:00 +0300

    Edit Form v2

--- js/scripts.js
+++ js/scripts.js
@@ -105,7 +105,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	const modalTrigger = document.querySelectorAll('[data-modal]')
 	const modal = document.querySelector('.modal')
-	const modalCloseBtn = document.querySelector('[data-close]')
 
 	function openModal() {
 		modal.classList.add('show')
@@ -124,10 +123,8 @@ window.addEventListener('DOMContentLoaded', () => {
 		btn.addEventListener('click', openModal)
 	})
 
-	modalCloseBtn.addEventListener('click', closeModal)
-
 	modal.addEventListener('click', event => {
-		if (event.target === modal) {
+		if (event.target === modal || event.target.getAttribute('data-close') == "") {
 			closeModal()
 		}
 	})
@@ -138,7 +135,7 @@ window.addEventListener('DOMContentLoaded', () => {
 		}
 	})
 
-	const modalTimerId = setTimeout(openModal, 5000)
+	const modalTimerId = setTimeout(openModal, 30000)
 
 	function showModalByScroll() {
 		if (
@@ -243,7 +240,7 @@ window.addEventListener('DOMContentLoaded', () => {
 	const forms = document.querySelectorAll('form')
 
 	const message = {
-		loading: 'Загрузка',
+		loading: 'img/form/spinner.svg',
 		success: 'Спасибо! Скоро мы с Вами свяжемся',
 		failure: 'Что-то пошло не так...',
 	}
@@ -256,15 +253,18 @@ window.addEventListener('DOMContentLoaded', () => {
 		form.addEventListener('submit', event => {
 			event.preventDefault()
 
-			const statusMessage = document.createElement('div')
-			statusMessage.classList.add('status')
-			statusMessage.textContent = message.loading
-			form.append(statusMessage)
+			let statusMessage = document.createElement('img')
+			statusMessage.src = message.loading
+			statusMessage.style.cssText = `
+				display: block;
+				margin: 0 auto;
+			`
+			form.insertAdjacentElement('afterend', statusMessage)
 
 			const request = new XMLHttpRequest()
 			request.open('POST', 'server.php')
 
-			request.setRequestHeader('Content-type', 'application/json')
+			request.setRequestHeader('Content-type', 'application/json; charset=utf-8')
 			const formData = new FormData(form)
 
 			const object = {}
@@ -279,15 +279,37 @@ window.addEventListener('DOMContentLoaded', () => {
 			request.addEventListener('load', () => {
 				if (request.status === 200) {
 					console.log(request.response)
-					statusMessage.textContent = message.success
+					showThanksModal(message.success)
+					statusMessage.remove()
 					form.reset()
-					setTimeout(() => {
-						statusMessage.remove()
-					}, 2000)
 				} else {
-					statusMessage.textContent = message.failure
+					showThanksModal(message.failure)
 				}
 			})
 		})
+	}
+
+	function showThanksModal(message) {
+		const prevModalDialog = document.querySelector('.modal__dialog')
+
+		prevModalDialog.classList.add('hide')
+		openModal()
+
+		const thanksModal = document.createElement('div')
+		thanksModal.classList.add('modal__dialog')
+		thanksModal.innerHTML = `
+			<div class="modal__content">
+				<div class="modal__close" data-close>&times;</div>
+				<div class="modal__title">${message}</div>	
+			</div>
+		`
+
+		document.querySelector('.modal').append(thanksModal)
+		setTimeout(() => {
+			thanksModal.remove()
+			prevModalDialog.classList.add('show')
+			prevModalDialog.classList.remove('hide')
+			closeModal()
+		}, 4000)
 	}
 })
